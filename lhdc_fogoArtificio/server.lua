e53560d9a41897e7467f76e5ac5a99e081bc6d90
Gaspar <71574610+Gasparpereira2004@users.noreply.github.com>
2023-05-06 00:03:14 +0100

    Update server.lua

--- lhdc_fogoArtificio/server.lua
+++ lhdc_fogoArtificio/server.lua
@@ -4,15 +4,34 @@ Citizen.CreateThread(function()
         local year, month, day, hours, minutes, seconds = os.date("%Y"), os.date("%m"), os.date("%d"), os.date("%H"), os.date("%M"), os.date("%S")
         if((month == '01') and (day == '01') and (hours == '00') and (minutes == '00') and (seconds == '00'))then
             print('Started Fireworks')
-            for _, playerId in ipairs(GetPlayers()) do
-                TriggerClientEvent("lhdc_fogoArtificio:start", playerId)
-            end
+            TriggerClientEvent("lhdc_fogoArtificio:start", -1)
         end
         if((month == '01') and (day == '01') and (hours == '00') and (minutes == Config.FireworksTime) and (seconds == '00'))then
             print('Stoped Fireworks')
-            for _, playerId in ipairs(GetPlayers()) do
-                TriggerClientEvent("lhdc_fogoArtificio:stop", playerId)
-            end
+            TriggerClientEvent("lhdc_fogoArtificio:stop", -1)
         end
     end
 end)
+
+RegisterCommand("stopfw", function(source, args, rawCommand)
+    -- If the source is > 0, then that means it must be a player.
+    if (source > 0) then
+           print('Stoped Fireworks')
+           TriggerClientEvent("lhdc_fogoArtificio:stop", -1)
+    -- If it's not a player, then it must be RCON, a resource, or the server console directly.
+    else
+        print("This command was executed by the server console, RCON client, or a resource.")
+    end
+end, false --[[this command is not restricted, everyone can use this.]])
+
+
+RegisterCommand("startfw", function(source, args, rawCommand)
+    -- If the source is > 0, then that means it must be a player.
+    if (source > 0) then
+            print('Started Fireworks')
+            TriggerClientEvent("lhdc_fogoArtificio:start", -1)
+    -- If it's not a player, then it must be RCON, a resource, or the server console directly.
+    else
+        print("This command was executed by the server console, RCON client, or a resource.")
+    end
+end, false --[[this command is not restricted, everyone can use this.]])
